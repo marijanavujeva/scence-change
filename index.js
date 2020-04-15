f264c0a9018278c39200eae747e1a208ca7a3b36
marijanavujeva <56256493+marijanavujeva@users.noreply.github.com>
2020-04-15 16:39:05 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script>
+        $("#leafers").on("click", function(event) {
+            // Do something!
+            console.log("leafer clicked");
+            $("#leafers").attr("src", "https://www.kasandbox.org/programming-images/avatars/aqualine-seedling.png");
+        });
+        
+        $("#beach").on("click", function(event) {
+            // Do something!
+            console.log("beach clicked");
+            $("#beach").attr("src", "https://www.kasandbox.org/programming-images/landscapes/beach-waves-daytime.png");
+        });
+        
+        $("#hopper").on("click", function(event) {
+            // Do something!
+            console.log("beach clicked");
+            $("#hopper").attr("src", "https://www.kasandbox.org/programming-images/creatures/Hopper-Cool.png");
+        });
