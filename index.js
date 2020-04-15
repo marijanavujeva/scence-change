e82276816fa7a709cc5d7d451ca504f5fae3a187
marijanavujeva <56256493+marijanavujeva@users.noreply.github.com>
2020-04-15 17:06:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,27 +13,6 @@
 
   <meta charset="utf-8">
         <title>Project: Scene change</title>
-        <style>
-        #leafers {
-            position: absolute;
-            top: 213px;
-            left: 378px;
-        }
-        #hopper {
-            position: absolute;
-            top: 226px;
-            left: 30px;
-        }
-            
-        </style>
-    </head>
-    <body>
-
-    <img src="https://www.kasandbox.org/programming-images/landscapes/beach-with-palm-trees.png" id="beach">
-
-    <img src="https://www.kasandbox.org/programming-images/avatars/leafers-seedling.png" id="leafers">
-    
-    <img src="https://www.kasandbox.org/programming-images/creatures/Hopper-Happy.png" id="hopper">
     
     
 
